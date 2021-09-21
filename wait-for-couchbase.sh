939a5f4979eabe2057460d84e175bff8608a4f76
mikereiche <michael.reiche@couchbase.com>
2021-09-21 15:46:43 -0700

    JCBC-1880 - Update SDC version, fix wait_for_couchbase and fix banner.
    Update SDC version to 4.3.0-M3.
    Fix wait_for_couchbase script to ignore indexes in other buckets.
    Fix banner to read "Spring Data Travel Sample" (vs Java Travel Sample)

--- wait-for-couchbase.sh
+++ wait-for-couchbase.sh
@@ -100,7 +100,7 @@ wait-for $ATTEMPTS \
   ":9102/api/v1/stats" \
   '.indexer.indexer_state == "Active"' \
   '. | keys | contains(["travel-sample:def_airportname", "travel-sample:def_city", "travel-sample:def_faa", "travel-sample:def_icao", "travel-sample:def_name_type", "travel-sample:def_primary", "travel-sample:def_route_src_dst_day", "travel-sample:def_schedule_utc", "travel-sample:def_sourceairport", "travel-sample:def_type", "travel-sample:inventory:airline:def_inventory_airline_primary", "travel-sample:inventory:airport:def_inventory_airport_airportname", "travel-sample:inventory:airport:def_inventory_airport_city", "travel-sample:inventory:airport:def_inventory_airport_faa", "travel-sample:inventory:airport:def_inventory_airport_primary", "travel-sample:inventory:hotel:def_inventory_hotel_city", "travel-sample:inventory:hotel:def_inventory_hotel_primary", "travel-sample:inventory:landmark:def_inventory_landmark_city", "travel-sample:inventory:landmark:def_inventory_landmark_primary", "travel-sample:inventory:route:def_inventory_route_primary", "travel-sample:inventory:route:def_inventory_route_route_src_dst_day", "travel-sample:inventory:route:def_inventory_route_schedule_utc", "travel-sample:inventory:route:def_inventory_route_sourceairport"])' \
-  '. | del(.indexer) | del(.["travel-sample:def_name_type"]) | map(.items_count > 0) | all' \
+  '. | del(.indexer) |  del(."travel-sample:def_name_type") | with_entries(select(.key|startswith("travel-sample:"))) | map(.items_count != 0) | all' \
   '. | del(.indexer) | map(.num_pending_requests == 0) | all'
 
 exec $@
